32464b1504e25e26a62d49618f2a61e9fa628270
chan18 <chan18@users.noreply.github.com>
2022-12-10 16:11:17 +0530

    adding login api with usernamd and password

--- API/Controllers/AccountsController.cs
+++ API/Controllers/AccountsController.cs
@@ -40,6 +40,29 @@ public class AccountsController : BaseApiController
         return user;
     }
 
+    [HttpPost("login")]
+    public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+    {
+        var user = await context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username);        
+        
+        if (user is null) return Unauthorized("Invalid username");
+
+        // password validation.
+        if (user.PasswordSalt is byte[] && user.PasswordHash is byte[])
+        {
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for (int i = 0; i < computedHash.Length; i++)
+            {
+                if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
+            }
+        }        
+
+        return user;
+    }
+
     private async Task<bool> UsersExists(string username)
     {
         return await context.Users.AnyAsync(user => user.UserName == username.ToLower());
