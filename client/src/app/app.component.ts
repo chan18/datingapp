a24561b3ddf3695f91e36ef6829da456a2852f5a
chan18 <chan18@users.noreply.github.com>
2022-12-10 23:20:00 +0530

    added persistent login with observables

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { User } from './models/user';
+import { AccountService } from './services/account.service';
 
 @Component({
   selector: 'app-root',
@@ -8,21 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title: string = 'Dating app';
-
-  // Todo:Fix: user interface/type
   user: any;
 
-  constructor(private http: HttpClient)
-  {
-    
-  }
+  constructor(private http: HttpClient, private accountService: AccountService)  { }
 
   ngOnInit(): void {
-    this.http.get("https://localhost:7001/api/users").subscribe({
+    this.getUsers(); 
+    this.setCurrentUser(); 
+  }
+
+  getUsers()
+  {
+    this.http.get<User>("https://localhost:7001/api/users").subscribe({
       next: (response) => this.user = response,
       error: error => console.log(error),
       complete: () => console.log("Requesrt completed")
     })
+  }
+
+  setCurrentUser()
+  {
+    const userString = localStorage.getItem('user');
+    if (!userString) return;    
+    const user: User = JSON.parse(userString);    
+    this.accountService.setCurrentUser(user);
   }
 
 }
--- client/src/app/services/account.service.ts
+++ client/src/app/services/account.service.ts
@@ -1,16 +1,39 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, map } from 'rxjs';
+import { User } from '../models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
   baseUrl = "https://localhost:7001/api/"
+  private currentUserSource = new BehaviorSubject<User | null>(null);
+  currentUser$ = this.currentUserSource.asObservable();
 
   constructor(private http: HttpClient) { }
 
   login(model: any)
   {
-    return this.http.post(this.baseUrl + "accounts/login", model);
+    return this.http.post<User>(this.baseUrl + "accounts/login", model).pipe(
+      map( (response: User) => {
+        const user = response;
+        if (user) {
+          localStorage.setItem('user',JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+      })
+    )
+  }
+
+  setCurrentUser(user: User)
+  {
+    this.currentUserSource.next(user);    
+  }
+
+  logout() 
+  {
+    localStorage.removeItem('user');
+    this.currentUserSource.next(null);
   }
 }
