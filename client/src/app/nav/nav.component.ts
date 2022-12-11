0b5282da9ef2547ac181e05ee0c895abf2e26ba8
chan18 <chan18@users.noreply.github.com>
2022-12-11 17:33:30 +0530

    routing with code on login

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable, of } from 'rxjs';
 import { User } from '../models/user';
 import { AccountService } from '../services/account.service';
@@ -11,16 +12,14 @@ import { AccountService } from '../services/account.service';
 export class NavComponent implements OnInit {
   model: any = {}
 
-  constructor(public accountService: AccountService) { }
+  constructor(public accountService: AccountService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
   login() {
     this.accountService.login(this.model).subscribe({
-      next: resposne => {
-        console.log(resposne);
-      },
+      next: _ =>  this.router.navigateByUrl("/members"),
       error: error => console.log(error)
     });
   }
@@ -28,6 +27,7 @@ export class NavComponent implements OnInit {
   logout()
   {
     this.accountService.logout();
+    this.router.navigateByUrl("/");
   }
 
 }
