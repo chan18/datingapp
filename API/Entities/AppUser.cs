8299d065bce6ba8b2bba00aa09b1cbd551f8bf2c
chan18 <chan18@users.noreply.github.com>
2022-12-12 00:12:06 +0530

    update user entity

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -10,4 +10,14 @@ public class AppUser
       public byte[]? PasswordHash { get; set; }
 
       public byte[]? PasswordSalt { get; set; }
+      public DateOnly DateOfBirth { get; set; }
+      public string KnownAs { get; set; } = "";
+      public DateTime Created { get; set; } = DateTime.UtcNow;
+      public DateTime LastActive { get; set; }      
+      public string Gender { get; set; } = "";
+      public string Introduction { get; set; } = "";
+      public string Interests { get; set; } = "";
+      public string City { get; set; } = "";
+      public string Country { get; set; } = "";
+      public List<Photo> Photos { get; set; } = new();
 }
