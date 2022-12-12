a5ee9de87318419f76f531c85f334f1b67eac442
chan18 <chan18@users.noreply.github.com>
2022-12-13 00:56:09 +0530

    added member details page and router navigation on click

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Member } from 'src/app/models/memeber';
+import { MembersService } from 'src/app/services/members.service';
 
 @Component({
   selector: 'app-member-detail',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./member-detail.component.css']
 })
 export class MemberDetailComponent implements OnInit {
+  member: Member | undefined;
 
-  constructor() { }
+  constructor(private memberService: MembersService, private route: ActivatedRoute) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {    
+    this.loadMember();
+  }
+
+  loadMember()
+  {
+    const username = this.route.snapshot.paramMap.get('username');
+
+    if (!username) return;
+
+    this.memberService.getMember(username).subscribe({
+      next:member => this.member = member,
+    })
   }
 
 }
--- client/src/app/services/members.service.ts
+++ client/src/app/services/members.service.ts
@@ -18,6 +18,6 @@ export class MembersService {
 
   getMember(username: string)
   {
-    return this.http.get<Member>(this.baseUrl + 'user/' + username);
+    return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
 }
