e0d4848b6b4c3272200c6969df11a5c14b56eb44
chan18 <chan18@users.noreply.github.com>
2022-12-13 01:31:14 +0530

    added tabs component in member details component

--- client/src/app/modules/shared.module.ts
+++ client/src/app/modules/shared.module.ts
@@ -1,8 +1,7 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { ToastrModule } from 'ngx-toastr';
-
+import { TabsModule } from 'ngx-bootstrap/tabs';
 
 @NgModule({
   declarations: [],
@@ -11,10 +10,13 @@ import { ToastrModule } from 'ngx-toastr';
     ToastrModule.forRoot({
       positionClass: 'toast-bottom-right'
     }),
+    TabsModule.forRoot(),
   ],
   exports: [
     BsDropdownModule,
-    ToastrModule
+    ToastrModule,
+    TabsModule
   ]
 })
+
 export class SharedModule { }
